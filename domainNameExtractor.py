3f4ecd17cccd630f0b4e0a50ea534de51454a6ed
ToddGallegos <toroga@hotmail.com>
2022-12-31 01:20:37 -0800

    so close

--- domainNameExtractor.py
+++ domainNameExtractor.py
@@ -12,18 +12,27 @@ def domain_name(url):
     leftpoint = 0
     rightpoint = 0
     for i in range(len(url)):
-        if url[i] == "/" and url[i-1] == "/" and url[i+1 != "w"]:
+        if leftpoint and rightpoint:
+            for i in range(leftpoint, rightpoint + 1):
+                result += url[i]
+            return result
+        if url[i] == "/" and url[i-1] == "/" and url[i+1] != 'w' and url[i+2] != 'w':
             leftpoint = i+1
             for j in range(i+2, len(url)):
                 if url[j] == ".":
                     rightpoint = j - 1
                     break
-        elif url[i] == 'w' and url[i+1] == '.':
-            leftpoint = i+2
+        elif leftpoint != True and url[i] == '.':
+            leftpoint = i+1
             for j in range(i+2, len(url)):
                 if url[j] == ".":
                     rightpoint = j - 1
                     break
+    if bool(rightpoint) == False:
+        rightpoint = len(url) - 1
+        ## OR THIS BELOW, DEPENDING ON CASE ##
+    #       rightpoint = leftpoint - 2
+    #       leftpoint = 0
     for i in range(leftpoint, rightpoint + 1):
         result += url[i]
     return result
@@ -32,3 +41,6 @@ def domain_name(url):
 print(domain_name("http://google.com"))
 print(domain_name("http://google.co.jp"))
 print(domain_name("www.xakep.ru"))
+print(domain_name("http://www.codewars.com"))
+print(domain_name("123.com"))
+print(domain_name('https://123'))
